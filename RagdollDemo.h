0a7c14d47451a867c5cd057296031a32a64882a4
ReemAlattas <ralattas@live.com>
2016-09-18 19:02:53 -0400

    assignment 8 - step 4

--- RagdollDemo.h
+++ RagdollDemo.h
@@ -65,7 +65,7 @@ class RagdollDemo : public GlutDemoApplication
     
     bool oneStep;
     
-    int IDs[10];
+    int IDs[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     
     
     void CreateBox(int index,
